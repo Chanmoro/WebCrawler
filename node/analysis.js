f6688ed4d6cb28b1e6794d7e4c7770c7e420fbc0
Kazuki Morozumi <kazuki.m777@gmail.com>
2015-01-09 12:50:17 +0900

    add files

---
+++ node/analysis.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var jschardet = require('jschardet');
+var iconv  = require('iconv').Iconv;
+
+if (process.argv.length !== 3) {
+	console.log('wrong argument.');
+	return;
+}
+
+var fileName = process.argv[2];
+var targetHtml = fs.readFileSync(fileName);
+
+// Encoding to utf8 string.
+var detectResult = jschardet.detect(targetHtml);
+var conv = new iconv(detectResult.encoding, 'UTF-8//TRANSLIT//IGNORE');
+var htmlStringUtf8 = conv.convert(targetHtml).toString();
+
+// Converting to jQuery object.
+$ = cheerio.load(htmlStringUtf8);
+
+// Extract information from html.
+var title = $('title').text();
+var description = $("meta[name=description]").attr("content");
+
+// Build tsv text.
+var record = title;
+record += "\t" + description;
+
+// Output to stdout.
+console.log(record);
---
+++ phantomjs/crawler.js
@@ -0,0 +1,35 @@
+// Read official documentation for more information .
+// http://phantomjs.org/api/
+var fs = require('fs');
+var system = require('system');
+var page = require('webpage').create();
+
+if(system.args.length !== 3){
+	console.log('wrong argument.');
+	phantom.exit(0);
+}
+
+// system.args[0] is this script file name.
+var targetUrl = system.args[1];
+var outputFilePath = system.args[2];
+
+// Access Url and rendering web page.
+page.open(targetUrl , function(status) {
+	if (status !== 'success') {
+		console.log('request failed!  URL: ' + targetUrl);
+		phantom.exit(1);
+	}
+	
+	// page.content stores the content of the rendered web page HTML.
+	var content = page.content;
+	
+	// Write to local file.
+	try {
+		fs.write(outputFilePath, content, 'w');
+	} catch(e) {
+		console.log(e);
+	}
+	
+	// You should call phantom.exits() to exits this program.
+	phantom.exit(0);
+});
